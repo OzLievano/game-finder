530213928371099ffb296cc43be828c8097f390b
OzLievano <Osvaldoalievano@gmail.com>
2024-04-23 00:05:16 -0400

    chore: added to do's

--- game-finder-app/src/components/NavigationBar.tsx
+++ game-finder-app/src/components/NavigationBar.tsx
@@ -6,7 +6,14 @@ export const NavigationBar = () => {
   const handleRedirect = (url: string) => {
     navigate(url);
   };
-
+  /*
+   TODO: Need to create "Create Account Page" 
+   TODO: Need to Hide create Match button unless user is authenticated
+   TODO: Create Match needs redirect to a form 
+   TODO: When user logs in , App Bar changes and has user icon to visit profile 
+   TODO: Notification System to notify user a match is confirmed to be played 
+   TODO: allow user to update match to change the status of the game Open, In Progress, Closed 
+  */
   return (
     <MuiAppBar>
       <MuiBox sx={{ display: "flex", justifyContent: "space-between" }}>
