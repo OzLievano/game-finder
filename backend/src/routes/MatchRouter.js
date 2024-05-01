1b546a0cdc8313220089007c4eb8442f147c746a
OzLievano <Osvaldoalievano@gmail.com>
2024-04-30 23:08:47 -0400

    feat: update match and get match requests API created

--- backend/src/routes/MatchRouter.js
+++ backend/src/routes/MatchRouter.js
@@ -5,10 +5,12 @@ const matchRouter = express.Router();
 matchRouter.use(express.json());
 
 matchRouter.get("/openMatchList", async (req, res) => {
+  const { displayName } = req.user;
+
   try {
     const matches = await db
       .collection("matches")
-      .find({ status: "open" })
+      .find({ gameStatus: "open", createdBy: { $ne: displayName } })
       .toArray();
     res.json(matches);
   } catch (error) {
@@ -41,6 +43,7 @@ matchRouter.post("/match", async (req, res) => {
     format: format,
     language: language,
     gameStatus: status,
+    requests: [],
   };
 
   try {
@@ -53,10 +56,45 @@ matchRouter.post("/match", async (req, res) => {
   }
 });
 
-matchRouter.put("/match/:id", (req, res) => {
-  res.send(
-    "This is an API to update the created match, mainly we will want to update the status "
-  );
+matchRouter.put("/match/:id", async (req, res) => {
+  const { id } = req.params;
+  const { requestId } = req.body;
+
+  const match = await db.collection("matches").findOne({ _id: id });
+  if (match) {
+    if (req.body.gameStatus === "accepted" && match.requests.length > 0) {
+      const request = match.requests.find((req) => req.requestId === requestId);
+      if (request) {
+        match.gameStatus = "accepted";
+        match.opponent = request.user;
+        match.requestId = request.requestId;
+        match.requests = []; // remove all requests after acceptance
+        await match.save();
+        res.json(match);
+      } else {
+        res.sendStatus(400).send("Invalid request ID");
+      }
+    } else {
+      res.sendStatus(400).send("Invalid game status");
+    }
+  } else {
+    res.sendStatus(404).send("Match not found");
+  }
+});
+
+matchRouter.get("/match/:id/requests", async (req, res) => {
+  const { id } = req.params;
+  const match = await db.collection("matches").findOne({ _id: id });
+  if (match) {
+    if (match.opponent) {
+      // If the match already has an opponent, remove all requests
+      match.requests = [];
+      await match.save();
+    }
+    res.json(match.requests);
+  } else {
+    res.sendStatus(404).send("Match not found");
+  }
 });
 
 export default matchRouter;
